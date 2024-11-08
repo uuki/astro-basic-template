ef50c471dfdc0f3b8e2936c4340855d908448e99
uuki <uuki.dev@gmail.com>
2024-11-09 00:32:30 +0900

    feat: migrate 4.15.9 and adjust environments

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,2 @@
-// Replace `astro/client` with `@astrojs/image/client`
-/// <reference types="@astrojs/image/client" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
