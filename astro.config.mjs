762425d53c4773b2b4fa132b78e6394584a6793f
uuki <uuki.dev@gmail.com>
2025-08-31 02:34:55 +0900

    feat: migrate 5.6.1 and adjust environments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
 import yaml from '@rollup/plugin-yaml';
 import commonjs from '@rollup/plugin-commonjs';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
-import postcssCustomMedia from 'postcss-custom-media';
-import postcssPresetEnv from 'postcss-preset-env';
 import svelte from '@astrojs/svelte';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -18,17 +15,7 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [
-    svelte(),
-    compress({
-      path: ['./dist'],
-      css: true,
-      html: false,
-      img: false,
-      js: false,
-      svg: false
-    })
-  ],
+  integrations: [svelte()],
   vite: {
     css: {
       preprocessorOptions: {
@@ -38,29 +25,13 @@ export default defineConfig({
           // Global addition styles
           additionalData: [`@use "sass:math";`].join('\n')
         }
-      },
-      postcss: {
-        plugins: [
-          postcssCustomMedia(),
-          postcssPresetEnv({
-            autoprefixer: {
-              flexbox: false,
-              grid: true
-            },
-            stage: 3,
-            features: {
-              'custom-properties': false
-            }
-          })
-        ]
       }
     },
-    plugins: [commonjs(), yaml(), sassGlobImports()],
+    plugins: [sassGlobImports(), commonjs(), yaml()],
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './'),
-        '@': path.resolve(__dirname, './src'),
-        '#': path.resolve(__dirname, './src/types')
+        '@': path.resolve(__dirname, './src')
       }
     }
   }
