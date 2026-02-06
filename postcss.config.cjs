3591a8f48fd791aae21fb8ffbacc44fd13d20579
uuki <uuki.dev@gmail.com>
2026-02-06 20:10:37 +0900

    chore: remove unnecessary line from postcss.config.cjs #4

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,9 +2,6 @@ const postcssGlobalData = require('@csstools/postcss-global-data');
 
 module.exports = {
   plugins: [
-    postcssGlobalData({
-      files: ['./src/styles/foundations/custom-media.scss']
-    }),
     require('postcss-custom-media'),
     require('postcss-preset-env')({
       stage: 3,
